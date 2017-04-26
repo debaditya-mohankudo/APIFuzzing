47551621413d41a7acd4a326e1d688474527b8c3
Debaditya M <dmohankudo@users.noreply.github.com>
2017-04-26 11:45:08 +0530

    Add files via upload

---
+++ TestAPI_EE_git.py
@@ -0,0 +1,109 @@
+'''
+Set Up:
+    ::python2 or python 3(preferred) flavor of  (miniconda) installed
+Usage:
+    ::mal_file -> a file name containing malicious strings: 'xss.txt'
+                  OR
+               -> a list of values haing malicious strings : ["alert}>'><script>alert(<fin2000>)</script>", "<script>alert(<fin2000>)</script>", ...]
+               Each malicious string is set for all the keys in the json structure and posted
+
+Dependency: UtilsLib.py in the same directory as that of script
+'''
+from requests.exceptions import ConnectionError
+
+from  UtilsLib import Utils
+import time
+import requests
+import sys
+#####################################################################
+requests.packages.urllib3.disable_warnings()  # supress https warnings
+######################################################################
+a = [{
+      "partnerOrderId": "xxxxxx",
+    "productType": "SecureSiteStarter",
+    "csr": "dgdghdfh",
+    "serverType": "Apache",
+    "validityPeriodDays": 365,
+    "authType": "DNS",
+    "domain": {
+      "cn": "sfs.net",
+      "sans": None
+               },
+    "org": {
+      "orgName": "xxxxxx",
+      "orgUnit": "Eng",
+      "address": {
+        "addressLine1": "4201 norwalk dr1",
+        "addressLine2": "",
+        "addressLine3": "",
+        "phoneNumber": "",
+        "city": "san jose",
+        "state": "california",
+        "country": "us",
+        "zip": "95129"
+      }
+      }
+
+
+  ,
+       "certTransparency":{
+        "ctLogging":False
+     },
+
+    "signatureAlgorithm": "sha256WithRSAEncryption",
+    "certChainType": "MIXED",
+    "locale": None
+  }
+]
+######################################################################
+u = Utils()  # create instance
+#######################################################################
+authType = 'Basic '
+auth_token = 'ssgsgsgsdhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'
+api_url_enroll = 'https://xxxxxx.net/ssl/v1/enroll'
+#mal_file = ["alert}>'><script>alert(<fin2000>)</script>"]
+mal_file = 'xss.txt'
+########################################################################
+time_i = str(time.ctime()).replace(' ', '-').replace(':', '-')
+enroll_fail_file = 'Enroll_Fail_' + time_i + '.txt'
+enroll_pass_file = enroll_fail_file.replace('Fail', 'Pass')
+network_issues_file = enroll_fail_file.replace('Fail', 'network_issues')
+
+open(enroll_fail_file, 'w').close()  # create the files if does not exist
+open(enroll_pass_file, 'w').close()  # create teh file if does not exist
+open(network_issues_file, 'w').close()
+import socks, socket
+
+if True:
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 8899)
+    socket.socket = socks.socksocket
+########################################################################
+counter = 0
+for postdata, mal_string, key in u.postdata_generator_with_insecure_values_ee(a, mal_file):
+    counter+=1
+    print(key, postdata, counter)
+    try:
+        resp = requests.post(api_url_enroll,
+                             json=postdata,
+                             verify=False,
+                             headers={'Authorization': authType + auth_token})
+
+        if resp.status_code in [200, 201]:
+            outputfile = enroll_pass_file
+        else:
+            outputfile = enroll_fail_file
+        print(resp.status_code)
+
+        u.write_details_to_file_ee(outputfile,
+                                   '=BEGIN=' * 5,
+                                   'Post url::' + api_url_enroll,
+                                   'POST Data::' , postdata,
+                                   'malicious string :original key-value::' + mal_string +':' + key,
+                                   'POST response : Here is output::'+ resp.text,
+                                   'Status Code::' + str(resp.status_code),
+                                   '=END=' * 5)
+    except ConnectionError:
+        print('-' * 20 )
+        print('----Connection Issues---')
+        print('-' * 20 )
+        #sys.exit(-1)
---
+++ UtilsLibFuzzing.py
@@ -0,0 +1,159 @@
+"""
+Set up: Works with python 2 or python 3 version  miniconda(out of box)
+Usage: take an input dictionary/json ( all valid inputs)
+      return an dictionary/json ( with one value replaced by string
+      from the xss/sql string file)
+      Works for commercial/EE apis
+Note: 'yield' is heavily used, helps to separate test data creation logic from use
+
+
+__maintainer__: debaditya
+"""
+import pickle
+import os
+import time
+import copy
+import codecs
+
+class Utils(object):
+    """docstring for Utils"""
+
+    def __init__(self):
+        pass
+
+
+    def postdata_generator_with_insecure_values(self, filename, input_dict, specific_params=None):
+        parameters_list = specific_params  if specific_params is not None else input_dict.keys()
+        for parameter in parameters_list:
+            print('-' * 60)
+            print('parameter targeted : {param}'.format(param=parameter))
+            print('-' * 60)
+            for value in generate_insecure_strings(filename):
+                    print('-' * 20)
+                    print(value)
+                    print('-' * 20)
+                    output_dict = input_dict.copy()
+                    output_dict[parameter] = value
+                    yield output_dict
+
+
+    def generate_insecure_strings(self, mal_data):
+        # yield is for lazy binding -> iterator pattern
+        # http://stackoverflow.com/questions/2223882/whats-different-between-utf-8-and-utf-8-without-bom
+        # encoding used to remove BOM char in mal input issue
+        if isinstance(mal_data, str):
+            if os.path.isfile(mal_data):
+                for line in codecs.open(mal_data, 'r', encoding="utf-8-sig").readlines():
+                    data = line.rstrip('\n').rstrip('\r')
+                    if data != '':
+                        yield data
+        else:
+            for data in mal_data:
+                    yield data
+
+
+
+    def write_details_to_file(self, postdata, resp, filename):
+        to_write = ''
+        to_write += '\n' + repr(postdata) + '\n' + '-' * 40 + '\n'
+        to_write += '"' + resp + '"'
+        print(to_write)
+        with open(filename, 'a') as f:
+            f.write(str(to_write))
+
+
+    def write_details_to_file_ee(self, filename, *args):
+        for arg in args:
+            to_write = ''
+            to_write += '\n' + repr(arg) + '\n' + '-' * 40 + '\n'
+            with open(filename, 'a') as f:
+                f.write(str(to_write))
+
+
+    def _serialize_data(self, data_object, pickle_file):
+        with open(pickle_file, 'wb') as f:  # hard coded pickle name??
+            pickle.dump(data_object, f)
+
+
+    def _deserialize_data(self, pickle_file):
+        with open(pickle_file, 'rb') as f:
+            return pickle.load(f)
+
+
+    def parse_json_get_items(self, a_object,
+                             str_malicious=None,
+                             key=None,
+                             get_item=True,
+                             set_item=False):
+        ''' this function does two operations
+        1. When get_item is true -> gets all key value pairs in self.list_k,
+        2. When set_item is true -> sets the value for one key to malicious
+        3. When there are duplicate keys like zip, try giving unique values
+        '''
+
+        if not hasattr(self, 'done_set_item'):
+            self.done_set_item = False  # flag if the malicious value is set- one time creation
+        if not hasattr(self, 'list_k'):
+                        self.list_k = []  # stores all key-val pairs - one time execution
+
+        if not self.done_set_item:  # if set quit the function
+            if isinstance(a_object, list):
+                for item in a_object:
+                    if isinstance(item, (list, dict)): # can list appear in list?
+                        self.parse_json_get_items(item, str_malicious, key,
+                                                  get_item, set_item)
+
+            elif isinstance(a_object, dict):
+                for item in a_object:
+                    if isinstance(a_object[item], (dict, list)):
+                        self.parse_json_get_items(a_object[item], str_malicious,
+                                                  key, get_item, set_item)
+                    else:
+                        self.parse_json_get_items((item, a_object[item], a_object),
+                                                  str_malicious,
+                                                  key, get_item,
+                                                  set_item)
+
+            elif isinstance(a_object, tuple): # each item in  a dict is a tuple
+                key1 = str(a_object[0]) + '-' + str(a_object[1])
+                if get_item is True:
+                    if key1 not in self.list_k:
+                        self.list_k.append(key1)
+
+                elif set_item is True and not self.done_set_item:
+                    if key == key1:
+                        a_object[2][a_object[0]] = str_malicious
+                        self.done_set_item = True
+
+
+        self.done_set_item = False # reset the value after recursion is complete
+
+    def parse_json_set_items(self, a_object,
+                             str_malicious=None,
+                             key=None,
+                             get_item=False,
+                             set_item=True):
+        self.parse_json_get_items(a_object,
+                             str_malicious,
+                             key,
+                             get_item=False,
+                             set_item=True)
+
+
+    def generate_testdata_with_malicious_str_ee(self, a_json_list, str_malicious):
+        self.parse_json_get_items(a_json_list) # fills the self.list_k
+        for key in self.list_k:
+            input_json = copy.deepcopy(a_json_list) #http://stackoverflow.com/questions/184643/what-is-the-best-way-to-copy-a-list
+            self.parse_json_set_items(input_json, str_malicious, key, get_item=False, set_item=True)
+            yield input_json, key
+
+    def postdata_generator_with_insecure_values_ee(self, a_json_list, mal_data):
+        for value in self.generate_insecure_strings(mal_data):
+            for td, key in self.generate_testdata_with_malicious_str_ee(a_json_list, value):
+                yield td, value, key
+
+    def postdata_generator_with_insecure_values_get_req_ee(self, api_url_get, mal_data, target_param):
+        for value in self.generate_insecure_strings(mal_data):
+            if target_param != []:
+                for param in target_param:
+                    yield value, api_url_get.replace(param, value)
